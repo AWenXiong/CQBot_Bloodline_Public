38805903a3e994f2d8a82e5fae3b8931196ed8a6
dollybelu <502063298@qq.com>
2019-08-18 23:10:43 +0800

    预留了配置敌阵容的接口

--- src/main/java/com/cq/httpapi/demo/dto/SZJ/Request/BackEndDataRequest/CreateEnemyInfoRequest.java
+++ src/main/java/com/cq/httpapi/demo/dto/SZJ/Request/BackEndDataRequest/CreateEnemyInfoRequest.java
@@ -1,4 +1,16 @@
 package com.cq.httpapi.demo.dto.SZJ.Request.BackEndDataRequest;
 
+import java.util.ArrayList;
+
 public class CreateEnemyInfoRequest {
+
+    public ArrayList<CreateEnemyInfoRequestLevel> Level;
+
+    public ArrayList<CreateEnemyInfoRequestLevel> getLevel() {
+        return Level;
+    }
+
+    public void setLevel(ArrayList<CreateEnemyInfoRequestLevel> level) {
+        Level = level;
+    }
 }
--- src/main/java/com/cq/httpapi/demo/dto/SZJ/Response/BackEndDataResponse/CreateInvitationCodeResponse.java
+++ src/main/java/com/cq/httpapi/demo/dto/SZJ/Response/BackEndDataResponse/CreateInvitationCodeResponse.java
@@ -3,4 +3,7 @@ package com.cq.httpapi.demo.dto.SZJ.Response.BackEndDataResponse;
 import com.cq.httpapi.demo.dto.SZJ.SZJResponse;
 
 public class CreateInvitationCodeResponse extends SZJResponse {
+
+    public Integer Id;
+
 }
