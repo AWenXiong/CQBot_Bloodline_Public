5f8d7a9ab82e3e7c368c032d964a2d1feef64d00
dollybelu <502063298@qq.com>
2019-08-18 23:14:04 +0800

    改错了

--- src/main/java/com/cq/httpapi/demo/dto/SZJ/Response/BackEndDataResponse/CreateInvitationCodeResponse.java
+++ src/main/java/com/cq/httpapi/demo/dto/SZJ/Response/BackEndDataResponse/CreateInvitationCodeResponse.java
@@ -3,7 +3,4 @@ package com.cq.httpapi.demo.dto.SZJ.Response.BackEndDataResponse;
 import com.cq.httpapi.demo.dto.SZJ.SZJResponse;
 
 public class CreateInvitationCodeResponse extends SZJResponse {
-
-    public Integer Id;
-
 }
