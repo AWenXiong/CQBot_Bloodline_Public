7bd0f3bfa83f4742e86b71c82cafe27e3549b817
dollybelu <502063298@qq.com>
2019-08-18 23:13:05 +0800

    预留了配置敌阵容的接口

--- src/main/java/com/cq/httpapi/demo/dto/SZJ/Response/BackEndDataResponse/CreateEnemyInfoResponse.java
+++ src/main/java/com/cq/httpapi/demo/dto/SZJ/Response/BackEndDataResponse/CreateEnemyInfoResponse.java
@@ -3,4 +3,5 @@ package com.cq.httpapi.demo.dto.SZJ.Response.BackEndDataResponse;
 import com.cq.httpapi.demo.dto.SZJ.SZJResponse;
 
 public class CreateEnemyInfoResponse extends SZJResponse {
+    public Integer Id;
 }
